c273781bc3f8d9b05004f9c47b5b6db6b97f1c20
Rebecca Brown <rebecca.brown@msn.com>
2022-08-03 18:54:31 -0400

    create, nav, hero and footer partials

---
+++ scripts/index-page.js
@@ -0,0 +1,9 @@
+
+
+//create element
+//customize element
+//append element
+
+// const cardContent = document.createElement("div");
+// cardContent.classList.add("card__content");
+// card.appendChild(personCard)
